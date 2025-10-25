967d232b66c7c6da717209922f042fbe61fb122f
cyeji <98408267+cyeji@users.noreply.github.com>
2025-10-25 16:49:19 +0900

    feat : 기능 요구사항 구현
    - 주어진 횟수 동안 n대의 자동차는 전진 또는 멈출 수 있다.
    - 각 자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.
    - 자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.
    - 사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.
    - 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
    - 자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다. 우승자는 한 명 이상일 수 있다.
    - 우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,73 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] racingCars = Console.readLine().split(",");
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        int tryCount = Integer.parseInt(Console.readLine());
+
+        Queue<RacingCarCount> queue = new LinkedList<>();
+        for (String racingCar : racingCars) {
+            queue.offer(new RacingCarCount(racingCar, 0));
+        }
+
+        for (int i = 0; i < tryCount; i++) {
+            int size = queue.size();
+            while (size != 0) {
+                RacingCarCount poll = queue.poll();
+                int count = poll.count;
+                int randomCount = Randoms.pickNumberInRange(0, 9);
+                if (randomCount >= 4) {
+                    System.out.println(poll.name + " : " + "-".repeat(count + 1));
+                    queue.offer(new RacingCarCount(poll.name, count + 1));
+                } else {
+                    System.out.println(poll.name + " : " + "-".repeat(count));
+                    queue.offer(new RacingCarCount(poll.name, count));
+                }
+                size--;
+            }
+            System.out.println();
+        }
+
+        List<RacingCarCount> list = new ArrayList<>(queue);
+        Collections.sort(list);
+
+        StringBuilder result = new StringBuilder();
+        int max = Integer.MIN_VALUE;
+        for (RacingCarCount racingCarCount1 : list) {
+            if (racingCarCount1.count >= max) {
+                result.append(racingCarCount1.name);
+                max = racingCarCount1.count;
+                result.append(",");
+            }
+        }
+        result.deleteCharAt(result.length() - 1);
+
+        System.out.println("최종 우승자 : " + result);
+
+    }
+
+    public static class RacingCarCount implements Comparable<RacingCarCount> {
+        private String name;
+        private int count;
+
+        public RacingCarCount(String name, int count) {
+            this.name = name;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(RacingCarCount o) {
+            return o.count - this.count;
+        }
     }
 }
